79239f79e87ac8b5f09fde087c6fe4d33233f96e
Sarah Gershuni <s0556726780@gmail.com>
2025-04-06 16:50:48 +0300

    document and user schemas

---
+++ back/models/document.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const DocumentSchema   = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+
+        // Store as HTML for text formatting (e.g., <b> for bold, <i> for italics)
+        content: { type: String,
+            required: true
+        }
+
+    },
+    {
+        // Enable timestamps for createdAt and updatedAt fields
+        timestamps: true,
+    })
+
+module.exports = mongoose.model('Document', DocumentSchema)
